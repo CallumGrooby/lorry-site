feaa7d4733526089ed87025b2e152b98e0f9bcae
Callum <cg5290g@gre.ac.uk>
2024-01-13 18:22:43 +0000

    added more to the homepage

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,19 +1,56 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
 
-export const HeroSection = ({ title, text, img, hasButton, buttonText }) => {
+export const HeroSection = ({ altStyle ,title, text, img, hasButton, buttonText }) => {
   return (
-    <section className='hero-section' style={{backgroundImage : `url(${img})`}}>
-       <div className='content'>
-          <h1>{title}</h1>
-          <p>{text}</p>
-          {hasButton ?  <button className='action-button'>{buttonText}</button>:<></>}
-       </div>
+    <section className= 'hero-section' 
+      style={{backgroundImage : `url(${img})`,
+      objectFit: 'contain',
+      backgroundRepeat:'no-repeat',
+      backgroundSize: 'cover',
+      alignItems: `${altStyle ? 'flex-end' : 'flex-start'}`
+    }}>
+
+    {altStyle && (
+      <div className='iconed-area'>
+          <span className='background'>
+
+          <div className='icons'>
+            <div className='iconed-stat'>
+              <FontAwesomeIcon className='icon' icon ="snowflake" />
+              <h1>Founded In 1969</h1>
+            </div>
+
+            <div className='iconed-stat'>
+              <FontAwesomeIcon className='icon' icon ="snowflake" />
+              <h1>Founded In 1969</h1>
+            </div>
+
+            <div className='iconed-stat'>
+              <FontAwesomeIcon className='icon' icon ="snowflake" />
+              <h1>Founded In 1969</h1>
+            </div>
+            
+          </div>
+          
+
+          </span>
+      </div>)}
+
+    <div className='content'>
+      <h1>{title}</h1>
+      <p>{text}</p>
+      {hasButton ?  <button className='action-button'>{buttonText}</button>:<></>}
+    </div>
        
+
+
     </section>
   );
 };
 
 HeroSection.propTypes = {
+  altStyle : PropTypes.bool,
   title: PropTypes.string,
   text: PropTypes.string,
   img: PropTypes.string,
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import { NavigationBar } from "../components/NavigationBar"
 import { HeroSection } from "../components/HeroSection"
-import image from '../assets/lorry-hero01.png'
-
+import lorryHeroImage from '../assets/lorry-hero01.png'
+import packingAboutUsImage from '../assets/packing-aboutus.png'
+import flowersHeroImage from '../assets/flower-hero.png'
+import lorryJoinUsImage from '../assets/lorry-joinus.png'
+import warehouseGroupsImage from '../assets/warehouse-groups.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { GoogleMap } from "../components/GoogleMap"
 
 
 
@@ -11,9 +15,10 @@ export const Home = () => {
     <section>
         <NavigationBar />
         <HeroSection 
-            title={"Your Premier Partner in Temperature-Controlled Supply Chain Management Across the UK and Europe"}
-            text={""}
-            img={image}
+            altStyle={false}
+            title={"Elevate Your Supply Chain with FreshLinc"}
+            text={"Welcome to FreshLinc, your premier partner in temperature-controlled supply chain management. As a leading service provider across the UK and Europe, we specialize in the seamless handling of fresh, chilled, and horticultural products. Our state-of-the-art facilities and a modern fleet are geared towards delivering a cost-effective and highly efficient service, tailored to meet the unique needs of our customers. At FreshLinc, we don't just manage supply chains; we elevate them."}
+            img={lorryHeroImage}
             hasButton={true}
             buttonText={"Request A Quote"}
         />    
@@ -21,9 +26,21 @@ export const Home = () => {
       <IconedSection />
       <AboutUsSection></AboutUsSection>
 
+      <HeroSection
+        altStyle={true}
+        title={"Our Journey: Established, Expanded, and Excelling."}
+        text={""}
+        img={flowersHeroImage}
+        hasButton={true}
+        buttonText={"Request A Quote"}
+      ></HeroSection>
+
+      <JoinOurTeamSection></JoinOurTeamSection>
+      <GroupsSections></GroupsSections>
     </section>
   )
 }
+
 
 const IconedSection = () =>{
 
@@ -117,10 +134,75 @@ const AboutUsSection = ()=>{
         </div>
       </article>
 
-
+        <img  src={packingAboutUsImage} alt="warehouse image"></img>
 
         </section>
       </div>
     </>
   )
 }
+
+
+const JoinOurTeamSection = () =>{
+  return(
+    <section className="container">
+      <section className="join-us-section">
+        <div className="content">
+          <h3>Our Team</h3>
+          <h1>Join Our Team</h1>
+          <p>At FreshLinc, our team is the driving force behind our success, providing market-leading logistical solutions with unwavering commitment to service. We prioritize the growth and development of our colleagues, offering diverse opportunities for skill enhancement through formal and vocational training. From Apprenticeships to Fast Track LGV training, we invest in your success.</p>
+          <button className='action-button'>Find out more</button>
+        </div>
+
+        <div className="image-container">
+          <img src={lorryJoinUsImage}></img>
+        </div>
+      </section>
+    </section>
+  );
+}
+
+const GroupsSections = ()=>{
+  return(
+      <section className="group-section">
+
+      <div className="image-container">
+        <img src={warehouseGroupsImage}></img>
+      </div>
+
+      <article className="groups-divisions">
+        <section className="offices-section">
+          <h3>Our Divisions</h3>
+          <h1>Fresh Lincs Groups</h1>
+          <p>The FreshLinc Group delivers a spectrum of logistics solutions and expertise tailored to industry demands. Explore our divisions for precision in storage, proficiency in transportation, and innovative supply chain management.</p>
+          <button className='action-button'>Group Divisions</button>
+        </section>
+
+        <section className="map-section">
+          <div className="information-holder">
+            <h1>Our Head Office</h1>
+            <p> 
+                FreshLinc Head Office <br/>
+                Wardentree Park<br/>
+                Pinchbeck<br/>
+                Spalding<br/>
+                Lincolnshire<br/>
+                PE11 3ZN
+            </p>
+
+            <p>Tel: +44 (0)1775 710900</p>
+            <p style={{color: '#f8b9bc'}}>information@freshlinc.com</p>
+          </div>
+          <div className="map-container-old">
+            
+          </div>
+          </section>
+
+      </article>
+
+      {/* <GoogleMap></GoogleMap> */}
+
+      </section>
+  )
+}
+
